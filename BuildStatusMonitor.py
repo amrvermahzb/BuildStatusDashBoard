b6746f5649a50883171f504d992d49e20b192c93
Mulder <john.j.mulder@philips.com>
2018-05-15 11:24:41 +0200

    buildmonitor with three build types per SW unit

---
+++ BuildStatusMonitor.py
@@ -0,0 +1,111 @@
+from tkinter import *
+from datetime import *
+import GetBuildStatus
+import os.path
+import time
+
+
+def get_build_result(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    result = "failed"
+    for line in content:
+        if "overall-result" in line:
+            if "success" in line:
+                result = "successful"
+    return result
+
+
+def get_build_dates_info(dates):
+    date_ci = dates[0]
+    date_ext_cvt = dates[1]
+    date_ext_cvg = dates[2]
+    days_ago_cvt = -1
+    days_ago_cvg = -1
+    if date_ci != "" and date_ext_cvt != "":
+        time_delta = date_ext_cvt - date_ci
+        days_ago_cvt = time_delta.days
+    if date_ci != "" and date_ext_cvg != "":
+        time_delta = date_ext_cvg - date_ci
+        days_ago_cvg = time_delta.days
+    return date_ci, date_ext_cvt, days_ago_cvt, date_ext_cvg, days_ago_cvg
+
+
+class BuildMonitor:
+    def __init__(self):
+        self.units = ["Acq", "AUI", "FSSys", "HostSW", "IDClient", "Infra", "IPSW", "SETool", "UI", "UIM", "View",
+                      "VxW"]
+        self.numUnits = 12
+        self.numBuildTypes = 3
+        self.buildLabels = [[0] * self.numBuildTypes for i in range(self.numUnits)]
+
+        self.root = Tk()
+        w, h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
+        self.root.geometry("%dx%d+0+0" % (w, h))
+        self.update()
+        self.root.mainloop()
+
+    def create_label(self, part, result, date_, days_ago, r, c):
+        text_date = date_.__str__()
+        if text_date != "":
+            text = part + " " + result + " (" + text_date + ")"
+            if days_ago < 0:
+                text = text + " *****"
+        else:
+            text = part + " " + result
+
+        var = StringVar()
+        var.set(text)
+        col = "red"
+        if result == "successful":
+            col = "green"
+        elif result == "not found":
+            col = "grey"
+        elif result == "not available":
+            col = "blue"
+        label = self.buildLabels[r-1][c-1]
+        if label == 0:
+            # create label only at first time
+            label = Label(self.root, textvariable=var, relief=RAISED, bg=col, width=43, height=3, font=("Helvetica", 17))
+            self.buildLabels[r - 1][c - 1] = label
+        else:
+            # else update label text
+            label.config(text=var)
+        label.grid(row=r, column=c)
+
+    def create_labels(self, name, results, date_info, row_index):
+        self.create_label(name + " CIBuild", results[0], date_info[0], 0, row_index, 1)
+        self.create_label(name + " Coverity", results[1], date_info[1], date_info[2], row_index, 2)
+        self.create_label(name + " Coverage", results[2], date_info[3], date_info[4], row_index, 3)
+
+    def update(self):
+        row = 1
+        now = time.strftime("(snapshot %A %B %d %H:%M)")
+        self.root.title("Allura R8.x.40 Build Status Monitor v3.0  " + now)
+        for unit in self.units:
+            build_info = GetBuildStatus.latest_unit_build_info(unit)
+            build_results = []
+            build_dates = []
+            for build_date, path in build_info:
+                if build_date == "<nodate>":
+                    build_dates.append("")
+                else:
+                    build_dates.append(datetime.strptime(build_date, "%Y-%m-%d").date())
+                if path == "<nopath>":
+                    build_results.append("not found")
+                else:
+                    build_file = path + r'\BuildInformation.xml'
+                    if os.path.isfile(build_file):
+                        build_results.append(get_build_result(build_file))
+                    else:
+                        build_results.append("not available")
+
+            date_info = get_build_dates_info(build_dates)
+            self.create_labels(unit, build_results, date_info, row)
+            row = row + 1
+
+        # refresh only once every 5 minutes
+        self.root.after(300000, self.update)
+
+
+monitor = BuildMonitor()
---
+++ GetBuildStatus.py
@@ -0,0 +1,42 @@
+from stat import ST_CTIME
+import os
+import time
+from time import strftime
+
+BUILD_BASE_PATH = r'\\code1.emi.philips.com\storage\IGTS_WIP_Storage'
+
+
+def latest_unit_build_info(unit):
+    path_ci = build_path(unit, "\CIBuilds\\")
+    path_ext_cvt = build_path(unit, "\ExtendedCIBuilds\\")
+    path_ext_cvg = build_path(unit, "\ExtendedCIBuilds\\", "_Coverage")
+    return latest_build_date_and_path(path_ci), \
+           latest_build_date_and_path(path_ext_cvt), \
+           latest_build_date_and_path(path_ext_cvg)
+
+
+def build_path(unit, build_type, post_fix=""):
+    base_path = BUILD_BASE_PATH + build_type + unit + r'\Allura_Win10_' + unit
+    path = base_path + r'_PreInt' + post_fix
+    if not os.path.isdir(path):
+        path = base_path + r'_Int' + post_fix
+        if not os.path.isdir(path):
+            path = "dir not found"
+    return path
+
+
+def latest_build_date_and_path(dir_path):
+    if dir_path == "dir not found":
+        return "<nodate>", "<nopath>"
+    # get all entries in the directory w/ stats
+    entries = (os.path.join(dir_path, fn) for fn in os.listdir(dir_path))
+    entries = ((os.stat(path), path) for path in entries)
+
+    # leave only regular files, insert creation date
+    entries = ((stat[ST_CTIME], path)
+               for stat, path in entries)
+    # NOTE: on Windows `ST_CTIME` is a creation date
+    #  but on Unix it could be something else
+    # NOTE: use `ST_MTIME` to sort by a modification date
+    entry = sorted(entries)[-1]
+    return strftime("%Y-%m-%d", time.strptime(time.ctime(entry[0]))), entry[1]
