15d418dcfab4708f6a16806d778724d63822f97a
CODE1\Philipsen, Niek <niek.philipsen@philips.com>
2018-11-27 15:06:03 +0100

    Made reviewingTestResult have similar implementation to ipislibTestResult

--- SquireMonitor/ipislibTestResult.py
+++ SquireMonitor/ipislibTestResult.py
@@ -1,13 +1,12 @@
 # Copyright (c) 2018 Koninklijke Philips N.V.
-from testResult import TestResult
+from viewingTestResult import ViewingTestResult
 import os.path
 import re
 
 
-class IpislibTestResult(TestResult):
+class IpislibTestResult(ViewingTestResult):
     def __init__(self, testSystem, testName):
         super().__init__(testSystem, testName)
-        self.resultFile = os.path.join(self.resultPath, 'test_summary.txt')
 
     def get_latest_result(self):
         if os.path.isfile(self.resultFile):
@@ -26,28 +25,3 @@ class IpislibTestResult(TestResult):
                 return "Not found"
         else:
             return "Not found"
-
-    def get_config(self):
-        return self.get_summary_element("CONFIG")
-
-    def get_result(self, systemMode):
-        summaryElement = self.get_summary_element(systemMode)
-        if summaryElement != "Not found":
-            testResultStr = "Overal TestResult ===>"
-            subStringContainingResult = summaryElement[testResultStr.__len__():]
-            return self.get_alpha_numeric_characters(subStringContainingResult)
-        return "Not found"
-
-    def get_summary_element(self, summaryElement):
-        returnValue = "Not found"
-        summaryElementStr = summaryElement + ":"
-        resultFile = open(self.resultFile, 'r')
-        for line in resultFile:
-            if summaryElementStr in line:
-                returnValue = line[summaryElementStr.__len__():]
-        resultFile.close()
-        return returnValue
-
-    def get_alpha_numeric_characters(self, line):
-        pattern = re.compile('\W')
-        return re.sub(pattern, '', line)
--- SquireMonitor/reviewingTestResult.py
+++ SquireMonitor/reviewingTestResult.py
@@ -1,36 +1,21 @@
 # Copyright (c) 2018 Koninklijke Philips N.V.
-from testResult import TestResult
+from viewingTestResult import ViewingTestResult
 import os.path
 import re
-import zipfile
 
 
-class ReviewingTestResult(TestResult):
+class ReviewingTestResult(ViewingTestResult):
     def __init__(self, testSystem, testName):
         super().__init__(testSystem, testName)
-        self.resultFile = os.path.join(self.resultPath, 'RevTestHarness_ur.TestReport')
-
-        self.zipFile = os.path.join(self.resultPath, 'TestReports.zip')
-        if not os.path.isfile(self.resultFile) and os.path.isfile(self.zipFile):
-            self.unzip_testresults()
 
     def get_latest_result(self):
         if os.path.isfile(self.resultFile):
-            resultFile = open(self.resultFile, 'r')
-            testResultStr = "Overal TestResult ===>"
-            for line in resultFile:
-                if testResultStr in line:
-                    subStringContainingResult = line[testResultStr.__len__():]
-                    return self.get_alpha_numeric_characters(subStringContainingResult)
-            return "Not found"
+            config = self.get_config()
+            if "MONOPLANE" in config:
+                return self.get_result("MONOPLANE")
+            elif "BIPLANE" in config:
+                return self.get_result("BIPLANE")
+            else:
+                return "Not found"
         else:
             return "Not found"
-
-    def unzip_testresults(self):
-        zip_ref = zipfile.ZipFile(self.zipFile, 'r')
-        zip_ref.extractall(self.resultPath)
-        zip_ref.close()
-
-    def get_alpha_numeric_characters(self, line):
-        pattern = re.compile('\W')
-        return re.sub(pattern, '', line)
